d4e173822890e322cf4b113bc7bad66005303945
rebleo <rml444@nyu.edu>
2019-04-08 18:02:37 -0400

    code

---
+++ week12/code/viewer/potato.js
@@ -0,0 +1,28 @@
+
+console.log("here!");
+// var blue = [];
+// $.getJSON( "theRGB.json", function( myData ) {
+// 	for (var i = 0; i < myData.colors.length; i++){
+// 		blue = myData.colors[i].blue;
+
+// 	}
+
+// 	var theElmnt = document.getElementById('title');
+
+//   for (var j = 0; j < blue.length; j ++){
+//   	console.log(blue[j].name);
+//   	theElmnt.append(blue[j].name);
+
+//   }
+ 
+
+// });
+
+
+var theFirst = document.getElementById("first");
+
+var theLast = document.getElementById("last");
+console.log(theFirst + theLast);
+
+
+
